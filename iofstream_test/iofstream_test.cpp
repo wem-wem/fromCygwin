fd6d106cf8d0b9104bb0c9e31c9ebf0554f963d2
rewish <spidertargal@yahoo.co.jp>
2015-02-27 23:29:28 +0900

    読み込み・書き込みテスト

--- iofstream_test/iofstream_test.cpp
+++ iofstream_test/iofstream_test.cpp
@@ -1,23 +1,28 @@
-// 読み込み・書き込みテスト.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
-//
-
 #include "stdafx.h"
+#include <iostream>
 #include <fstream>
 #include <string>
-#include <iostream>
 
-#define TEXT_NUM (4)
-
-std::ifstream inf("res/test.txt");
+std::ifstream inf("test.txt");    // インプットファイル
+std::ofstream outf("test.txt");// アウトプットファイル(ここで定義するとメモ帳の中身を空にしてくれているヨーダ)
+std::string str;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-  // 読み込みをテキストの数だけ繰り返す
-  for (int i = 0; i < TEXT_NUM; i++){
-    std::string str;
+  // メモ帳の内容を表示
+  for (int i = 0; i < 2; ++i){
     inf >> str;
     std::cout << str << std::endl;
   }
+
+  // メモ帳に"hoge"を書き込む
+  // std::ofstream outf("test.txt");  ここで定義したら上の表示でメモ帳の中身が消されることはなかった
+  str = "hoge";
+  outf << str;
+
+  // 再度メモ帳の内容を表示
+  inf >> str;
+  std::cout << str << std::endl;
 
   return 0;
 }
